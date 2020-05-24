05d5c6138f9403559f2bfd1ba903e8ec225bfe95
tjburton <tjburton@me.com>
2020-05-24 01:24:16 +0100

    Initial Commit

---
+++ months.py
@@ -0,0 +1,14 @@
+months = [
+    "Jan", "January",
+    "Feb", "February",
+    "Mar", "March",
+    "Apr", "April",
+    "May",
+    "Jun", "June",
+    "Jul", "July",
+    "Aug", "August",
+    "Sept", "September",
+    "Oct", "October",
+    "Nov", "November",
+    "Dec", "December"
+]
